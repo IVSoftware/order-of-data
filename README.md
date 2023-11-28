c9197bda7872b6fe79d20d8bec3bf91279578a7a
Tom Gregor <tgregor@ivsoftware.com>
2023-11-28 11:56:09 -0700

    https://stackoverflow.com/q/77563871/5438626

--- README.md
+++ README.md
@@ -19,5 +19,116 @@ class Record
 }
 ```
 
-[Placeholder]
+[![initial][1]][1]
 
+___
+For the sake of simplicity, suppose we're using `sqlite-net-pcl` instead of `System.Data.SQLite` so that `DataGridView` can use a straightforward list binding. _(This has no bearing on the actual answer, it just makes it easier to show the code.)_
+
+```
+BindingList<Record> Recordset { get; } = new BindingList<Record>();
+```
+
+The [Requery] button click handler is:
+
+```
+
+    buttonRequery.Click += async (sender, e) =>
+    {
+        Recordset.Clear();
+        using (var cnx = new SQLiteConnection(SQLitePath))
+        {
+            foreach (var item in cnx.Query<Record>("SELECT * FROM records ORDER BY Priority"))
+            {
+                Recordset.Add(item);
+            }
+        }
+    };
+```
+
+[![after drag drop][2]][2]
+
+In response, sort the priorities in order and apply them to the items in the new order they exist now before updating the database record.
+
+```
+dataGridView.MouseUp += (sender, e) =>
+{
+    var hit = dataGridView.HitTest(e.X, e.Y);
+    int dropRow = -1;
+    if (hit.Type != DataGridViewHitTestType.None)
+    {
+        dropRow = hit.RowIndex;
+        if (dragRow >= 0)
+        {
+            int tgtRow = dropRow + (dragRow > dropRow ? 1 : 0);
+            if (tgtRow != dragRow)
+            {
+                var record = Recordset[dragRow];
+                Recordset.Remove(record);
+                Recordset.Insert(tgtRow, record);
+                dataGridView.Refresh();
+                dataGridView.Rows[tgtRow].Selected = true;
+            }
+        }
+    }
+    if (dragLabel != null)
+    {
+        dragLabel.Dispose();
+        dragLabel = null;
+
+        // Apply the sorted priorities to the items as they now stand in the list.
+        var priorities = Recordset.Select(_ => _.Priority).OrderBy(_=>_).ToArray();
+        for(int index = 0; index < priorities.Length; index++)
+        {
+            Recordset[index].Priority = priorities[index];
+        }
+        using (var cnx = new SQLiteConnection(SQLitePath))
+        {
+            cnx.UpdateAll(Recordset);
+        }
+    }
+    dataGridView.CurrentCell = hit.ColumnIndex == -1 || hit.RowIndex == -1 ?
+        null :
+        dataGridView[hit.ColumnIndex, hit.RowIndex];
+};
+```
+___
+
+**Example**
+
+Suppose we have ability to do an ephemeral sort by clicking on the column header. As a test sequence, we can (ephemeral) sort by `Created`, then (ephemeral) sort on `Name`, before hitting the [Requery] button to retrieve the database items in the actual post-drag-drop order.
+
+```
+dataGridView.CellClick += (sender, e) =>
+{
+    if(e.RowIndex == -1)
+    {
+        Record[] sorted;
+        if (dataGridView.Columns[nameof(Record.Created)].Index == e.ColumnIndex)
+        {
+            sorted = Recordset.OrderBy(_ => _.Created).ToArray();
+        }
+        else if (dataGridView.Columns[nameof(Record.Name)].Index == e.ColumnIndex)
+        {
+            sorted = Recordset.OrderBy(_ => _.Name).ToArray();
+        }
+        else return;
+        Recordset.Clear();
+        foreach (var item in sorted)
+        {
+            Recordset.Add(item);
+        }
+    }
+};
+```
+
+[![test sequence][3]][3]
+___
+
+It follows that you can insert `Grape` between Apple and Banana by setting its priority to ((Apple.Priority) + (Banana.Priority)) / 2. 
+
+You get the idea.
+
+
+  [1]: https://i.stack.imgur.com/upAoM.png
+  [2]: https://i.stack.imgur.com/1fpcy.png
+  [3]: https://i.stack.imgur.com/UOc2b.png
--- order-of-data/MainForm.cs
+++ order-of-data/MainForm.cs
@@ -31,7 +31,6 @@ namespace order_of_data
                 Recordset.Clear();
                 UseWaitCursor = true;
                 await Task.Delay(500);
-                UseWaitCursor = false;
                 using (var cnx = new SQLiteConnection(SQLitePath))
                 {
                     foreach (var item in cnx.Query<Record>("SELECT * FROM records ORDER BY Priority"))
@@ -39,6 +38,8 @@ namespace order_of_data
                         Recordset.Add(item);
                     }
                 }
+                UseWaitCursor = false;
+                dataGridView.ClearSelection();
             };
         }
         string SQLitePath { get; } = Path.Combine(
@@ -146,11 +147,112 @@ namespace order_of_data
             };
         }
         BindingList<Record> Recordset { get; } = new BindingList<Record>();
-        Record[] _selectedRecords = new Record[0];
         int dragRow = -1;
         Label dragLabel = null;
     }
 }
+
+#if false
+protected override void OnLoad(EventArgs e)
+{
+    base.OnLoad(e);
+    dataGridView.AllowUserToAddRows = false;
+    dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+    using (var cnx = new SQLiteConnection(SQLitePath))
+    {
+        foreach (var item in cnx.Query<Record>("SELECT * FROM records ORDER BY Priority"))
+        {
+            Recordset.Add(item);
+        }
+    }
+    dataGridView.DataSource = Recordset;
+    dataGridView.Columns[nameof(Record.Created)].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+    dataGridView.Columns[nameof(Record.Created)].DefaultCellStyle.Format = @"hh\:mm\:ffffff";
+    dataGridView.Columns[nameof(Record.Name)].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+    BeginInvoke(() =>
+    {
+        dataGridView.ClearSelection();
+        dataGridView.Refresh();
+    });
+
+    dataGridView.CellMouseDown += (sender, e) =>
+    {
+        if (e.ColumnIndex < 0 || e.RowIndex < 0) return;
+        dragRow = e.RowIndex;
+        if (dragLabel == null) dragLabel = new Label();
+        dragLabel.Text = dataGridView[e.ColumnIndex, e.RowIndex].Value.ToString();
+        dragLabel.Parent = dataGridView;
+        dragLabel.Location = e.Location;
+    };
+    dataGridView.MouseMove += (sender, e) =>
+    {
+        if (e.Button == MouseButtons.Left && dragLabel != null)
+        {
+            dragLabel.Location = e.Location;
+            dataGridView.ClearSelection();
+        }
+    };
+    dataGridView.MouseUp += (sender, e) =>
+    {
+        var hit = dataGridView.HitTest(e.X, e.Y);
+        int dropRow = -1;
+        if (hit.Type != DataGridViewHitTestType.None)
+        {
+            dropRow = hit.RowIndex;
+            if (dragRow >= 0)
+            {
+                int tgtRow = dropRow + (dragRow > dropRow ? 1 : 0);
+                if (tgtRow != dragRow)
+                {
+                    var record = Recordset[dragRow];
+                    Recordset.Remove(record);
+                    Recordset.Insert(tgtRow, record);
+                    dataGridView.Refresh();
+                    dataGridView.Rows[tgtRow].Selected = true;
+                }
+            }
+        }
+        if (dragLabel != null)
+        {
+            dragLabel.Dispose();
+            dragLabel = null;
+            var priorities = Recordset.Select(_ => _.Priority).OrderBy(_=>_).ToArray();
+            for(int index = 0; index < priorities.Length; index++)
+            {
+                Recordset[index].Priority = priorities[index];
+            }
+            using (var cnx = new SQLiteConnection(SQLitePath))
+            {
+                cnx.UpdateAll(Recordset);
+            }
+        }
+        dataGridView.CurrentCell = hit.ColumnIndex == -1 || hit.RowIndex == -1 ?
+            null :
+            dataGridView[hit.ColumnIndex, hit.RowIndex];
+    };
+    dataGridView.CellClick += (sender, e) =>
+    {
+        if(e.RowIndex == -1)
+        {
+            Record[] sorted;
+            if (dataGridView.Columns[nameof(Record.Created)].Index == e.ColumnIndex)
+            {
+                sorted = Recordset.OrderBy(_ => _.Created).ToArray();
+            }
+            else if (dataGridView.Columns[nameof(Record.Name)].Index == e.ColumnIndex)
+            {
+                sorted = Recordset.OrderBy(_ => _.Name).ToArray();
+            }
+            else return;
+            Recordset.Clear();
+            foreach (var item in sorted)
+            {
+                Recordset.Add(item);
+            }
+        }
+    };
+}
+#endif
 
 [Table("records"), DebuggerDisplay("{Name}")]
 class Record
